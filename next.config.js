8eb7e314b9a0d7cbffad4a792485ede13ca039ac
David Mutua <mutuadavi@gmail.com>
2022-05-13 12:31:57 +0300

    deploy to heroku

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true,
+  // reactStrictMode: true,
 
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
